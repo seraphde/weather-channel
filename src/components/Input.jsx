7f68b0a26dcad725d1ef1283c1dd60d6802669a9
seraphde <103943807+seraphde@users.noreply.github.com>
2024-01-01 10:53:10 +0300

    i change  css

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,38 +1,33 @@
-import React from 'react'
+import React from 'react';
 import Location from './Location';
 import Icons from './icons';
 
-
 function Input() {
   return (
-   
-<div className='flex w-full mx-4 justify-center '>
- <div className='flex flex-row w1/4 item-center justify-center'>
-    <input type="text" className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize'
-      placeholder='search for city'
-/> 
-      <Location />
-      < Icons />
-   
- 
-</div>
-<div className='flex flexroww1/4 item-center justify-center'>
-<button name='metric' className='text-xl text-white font-light'>
-      °C 
-      </button>
-         <p className='text-xl text-white mx-1'>|</p>
-      <button name='imperial' className='text-white font-light text-xl'>
-      °F
-       </button>
+    <div className='flex w-full mx-4 justify-center'>
+      <div className='flex flex-row items-center justify-center w-full'>
 
+        <input
+          type="text"
+          className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize'
+          placeholder='Search for city'
+        />
+        <Location />
+        <Icons />
+      </div>
 
-</div>
-
-</div>
-
-            
-
+      <div className='flex flex-row items-center justify-center w-1/4'>
+       
+        <button name='metric' className='text-xl text-white font-light'>
+          °C
+        </button>
+        <p className='text-xl text-white mx-1'>|</p>
+        <button name='imperial' className='text-white font-light text-xl'>
+          °F
+        </button>
+      </div>
+    </div>
   );
 }
 
-export default Input
+export default Input;
